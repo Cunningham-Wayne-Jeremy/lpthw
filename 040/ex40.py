ed31ac78c0671e357274ca5281dfe0dc5f974de5
FOSS-ter <foss-ter@localhost.localdomain>
2020-01-05 15:43:10 -0500

    more notes on python

--- 040/ex40.py
+++ 040/ex40.py
@@ -1,8 +1,8 @@
 class Song(object):
-    
+
     def __init__(self,lyrics):
         self.lyrics = lyrics
-    
+
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
