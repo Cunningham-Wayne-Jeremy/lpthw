9248d796ab1c24b6a532b5b99af3f88176d1ebc0
Cunningham-Wayne-Jeremy <ultima62508@gmail.com>
2021-01-27 18:16:29 -0500

    More notes

--- 024/ex24.py
+++ 024/ex24.py
@@ -23,7 +23,7 @@ print(f"This should be five: {five}")
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars / 1000
+    crated = jars / 1000
     return jelly_beans, jars, crates
 
 start_point = 10000
--- 033/ex33.py
+++ 033/ex33.py
@@ -1,16 +1,18 @@
-i = 0
-numbers = []
+def userinputwhileloop():
 
-while i < 6:
-    print(f"At the top i is equal to {i}")
-    numbers.append(i)
-    i +=1
-    print("Numbers now: ", numbers)
-    print("At the bottom i is {}".format(i))
+    i=1
+    loopuntil= int(input("Enter an integer at which the loop will stop: "))
+    numbers = []
+    increment_value = 5#int(input("Enter a value the code increments by:"))
+    for i in range(0,loopuntil,increment_value):
+        print(f"At the top i is equal to {i}")
+        numbers.append(i)
+        print(f"What is the value of i? {i}")
+        print("Numbers now: ", numbers)
+        print("At the bottom i is {}".format(i))
 
-print("The numbers: ")
+        print("The numbers: ")
 
-for num in numbers:
-    print(num)
-
-    
+    for num in numbers:
+        print(num)
+userinputwhileloop()
